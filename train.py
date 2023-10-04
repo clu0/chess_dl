f0ee2b4937c6311f5fb4575b197c63dd26a45076
clu0 <chenlu96@gmail.com>
2023-10-04 00:07:52 +0000

    logging changes + model path change

--- train.py
+++ train.py
@@ -30,29 +30,30 @@ if __name__ == "__main__":
 
     assert os.path.exists(args.data_path)
     if args.load_model_path is not None:
-        assert os.path.exists(args.load_model_path)
+        assert os.path.exists(os.path.join(args.model_path, args.load_model_path))
     if not os.path.exists(args.model_path): os.makedirs(args.model_path)
     if not os.path.exists(args.loss_path): os.makedirs(args.loss_path)
 
     dataset_paths = [os.path.join(args.data_path, f) for f in os.listdir(args.data_path)]
-    
+
     # number of in channels should be T * 14 + 7 = 119 when T = 8
     # currently using T = 1, which means 21
     model = ChessNet(args.in_c, n_c=args.n_c, depth=args.depth, n_hidden=args.n_hidden)
     if args.load_model_path is not None:
         model.load_state_dict(torch.load(os.path.join(args.model_path, args.load_model_path)))
+        print(f"loaded model from {os.path.join(args.model_path, args.load_model_path)}")
     mse_loss_fn = nn.MSELoss()
     ce_loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters())
 
     if args.device == "cuda":
         model.cuda()
-    
+
     model.train()
-    
+
     mse_losses, ce_losses, total_losses = [], [], []
     for epoch in range(args.n_epoch):
-        print(f"starting epoch {epoch}")
+        print(f"starting epoch {epoch + args.n_prev_epoch}")
         start = time()
         all_mse_loss = 0
         all_ce_loss = 0
@@ -69,7 +70,7 @@ if __name__ == "__main__":
             print(f"training dataset {i} out of {len(dataset_paths)}")
             for state, action, value in dataloader:
                 state, action, value = state.to(args.device), action.to(args.device), value.to(args.device)
-                
+
                 optimizer.zero_grad()
                 p_model, v_model = model(state.float())
                 p_model = p_model.reshape(p_model.shape[0], -1)
@@ -79,22 +80,21 @@ if __name__ == "__main__":
                 loss: torch.Tensor = mse_loss + ce_loss
                 loss.backward()
                 optimizer.step()
-                
+
                 all_mse_loss += mse_loss.detach()
                 all_ce_loss += ce_loss.detach()
                 all_loss += loss.detach()
                 n_loss += 1
-        
+
         mse_losses.append(all_mse_loss / n_loss)
         ce_losses.append(all_ce_loss / n_loss)
         total_losses.append(all_loss / n_loss)
 
-        print(f"epoch {epoch}, mse loss {mse_losses[-1]}, ce loss {ce_losses[-1]}, loss {total_losses[-1]}, time {time() - start}")
-        if (epoch + 1) % 20 == 0:
+        print(f"epoch {epoch + args.n_prev_epoch}, mse loss {mse_losses[-1]}, ce loss {ce_losses[-1]}, loss {total_losses[-1]}, time {time() - start}")
+        if (epoch + 1) % 10 == 0:
             print(f"epoch {epoch + 1}, saving model")
             torch.save(model.state_dict(), os.path.join(args.model_path, f"model_{epoch + args.n_prev_epoch + 1}.pth"))
-    
+
             np.save(os.path.join(args.loss_path, "mse_loss.npy"), torch.tensor(mse_losses).to('cpu'))
             np.save(os.path.join(args.loss_path, "ce_loss.npy"), torch.tensor(ce_losses).to('cpu'))
             np.save(os.path.join(args.loss_path, "total_loss.npy"), torch.tensor(total_losses).to('cpu'))
-    
